d9f5227f18a748ec5a4cef02eb0a9c5b37741aff
city0409 <250924125@qq.com>
2018-01-15 16:18:28 +0800

    Initial commit

---
+++ Assets/DoTweenTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using System;
+
+public class DoTweenTest : MonoBehaviour 
+{
+    private Tweener ter;
+
+    private void Start () 
+	{
+        
+        Sequence seq = DOTween.Sequence();
+        ter = transform.DOMoveX(20, 2f);
+        seq.Append(ter);
+        seq.Append(transform.DORotate(new Vector3(0, 180, 0), 2));
+        seq.SetLoops(-1);
+        seq.Play();
+
+        transform.DOMove(new Vector3(5, 2, 2), 1).SetEase(Ease.OutQuint).SetLoops(1).OnComplete(myFunction);
+
+        
+    }
+
+    private void myFunction()
+    {
+        transform.GetComponent<Renderer>().material.DOColor(Color.red, 2);
+    }
+
+    private void Update () 
+	{
+		
+	}
+}
